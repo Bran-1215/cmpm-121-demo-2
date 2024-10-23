3ba5b1d887001d2208ecb443b21e0d13f7cc23ac
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2024-10-22 18:07:53 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,51 @@ appTitle.innerHTML = APP_NAME;
 app.append(appTitle);
 
 const appCanvas = document.createElement("canvas");
+appCanvas.height = 256;
+appCanvas.width = 256;
+const appCanvasContext = appCanvas.getContext("2d");
 app.append(appCanvas);
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+appCanvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+  });
+  
+  appCanvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+      drawLine(appCanvasContext, x, y, e.offsetX, e.offsetY);
+      x = e.offsetX;
+      y = e.offsetY;
+    }
+  });
+  
+  window.addEventListener("mouseup", (e) => {
+    if (isDrawing) {
+      drawLine(appCanvasContext, x, y, e.offsetX, e.offsetY);
+      x = 0;
+      y = 0;
+      isDrawing = false;
+    }
+  });
+  
+  function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+  }
+
+  const clearButton = document.createElement("button");
+  clearButton.innerHTML = "clear";
+  app.append(clearButton);
+  clearButton.addEventListener("click", () => {
+    appCanvasContext?.clearRect(0, 0, appCanvas.width, appCanvas.height);
+  });
